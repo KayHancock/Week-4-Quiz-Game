9b067dfd27ac7933ec83c5cd920d68321b5bd62d
Kayla Hancock <hancock.kae@gmail.com>
2022-06-24 22:09:02 -0400

    Updating structure, added css

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //Adding variables
+var time = document.getElementById("timer");
+var startBtn = document.getElementById("sbtn");
+var resetBtn = document.getElementById("rebtn");
+var highScore = document.getElementById("view");
+
+//Arrays
+
+//Functions
+function startGame () {
+    console.log("the start button works");
+}
+
+function resetGame () {
+    console.log("the reset button works");
+}
+
+
+//Listeners
+startBtn.addEventListener("click", startGame);
+resetBtn.addEventListener("click", resetGame);
